36f66156f060e50358be938dcd5a609b9cbc97fd
Claudia <octoclo@zoho.com>
2019-10-31 16:53:54 +0100

    Add Mesh Extrude + Camera Center tools

--- TheLastBeatUnity/Assets/_Project/Script/General/SceneSetup.cs
+++ TheLastBeatUnity/Assets/_Project/Script/General/SceneSetup.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEditor;
 
 public class SceneSetup : MonoBehaviour
 {
     [RuntimeInitializeOnLoadMethod]
-    public void Init()
+    public static void Init()
     {
         DOTween.Init();
+    }
+
+    [MenuItem("Tools/Bata/Centrer Camera")]
+    public static void CenterCam()
+    {
+        Camera target = SceneView.lastActiveSceneView.camera;
+        Transform tmp = target.transform;
+        tmp.position = Camera.main.transform.position;
+        tmp.rotation = Quaternion.LookRotation(Camera.main.transform.forward);
+        SceneView.lastActiveSceneView.AlignViewToObject(tmp);
     }
 }
