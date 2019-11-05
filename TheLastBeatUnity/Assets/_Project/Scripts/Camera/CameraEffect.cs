d31ca37b0b6b29096a084a3b0175e2ebe3f0bca4
Séverin Michaut <severinmichaut@gmail.com>
2019-11-05 14:43:56 +0100

    Implementing feature

--- TheLastBeatUnity/Assets/_Project/Scripts/Camera/CameraEffect.cs
+++ TheLastBeatUnity/Assets/_Project/Scripts/Camera/CameraEffect.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 using Sirenix.OdinInspector;
 using Cinemachine;
 
-[RequireComponent(typeof(Cinemachine.CinemachineVirtualCamera))] [ExecuteInEditMode]
+[RequireComponent(typeof(Cinemachine.CinemachineVirtualCamera))]
 public class CameraEffect : MonoBehaviour
 {
+    private void Start()
+    {
+        perlin = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        transposer = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineFramingTransposer>();
+    }
+
+    #region ScreenShake
     [TabGroup("ScreenShake")] [SerializeField]
     float intensityScreenShake;
 
@@ -18,14 +25,15 @@ public class CameraEffect : MonoBehaviour
 
     CinemachineBasicMultiChannelPerlin perlin;
 
+    [TabGroup("ScreenShake")][InfoBox("Ne fonctionne que si le mode solo de la camera est activé", InfoMessageType.None)][Button(ButtonSizes.Medium)]
+    public void Test()
+    {
+        StartScreenShake();
+    }
+
     public void StartScreenShake()
     {
         StartCoroutine(ScreenShakeSequence(screenShakeDuration));
-    }
-
-    private void Start()
-    {
-        perlin = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
     }
 
     IEnumerator ScreenShakeSequence(float duration)
@@ -41,4 +49,88 @@ public class CameraEffect : MonoBehaviour
             yield return null;
         }
     }
+    #endregion
+
+    #region Zoom
+
+    enum ZoomType
+    {
+        FOV,
+        Distance
+    };
+
+    [TabGroup("Zoom")] [SerializeField]
+    ZoomType modifierType;
+
+    [TabGroup("Zoom")] [SerializeField]
+    float durationZoom;
+
+    [TabGroup("Zoom")] [SerializeField]
+    AnimationCurve zoomOverTime;
+
+    [TabGroup("Zoom")] [SerializeField]
+    float valueForTest;
+
+    [TabGroup("Zoom")] [Button(ButtonSizes.Medium)] [InfoBox("Plus fluide si le mode solo est activé", InfoMessageType.None)]
+    public void TestZoom()
+    {
+        transposer = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineFramingTransposer>();
+        StartZoom(valueForTest);
+    }
+
+    CinemachineFramingTransposer transposer;
+    IEnumerator currentZooming;
+
+    public void StartZoom(float modifier)
+    {
+        modifier = Mathf.Abs(modifier);
+        Debug.Assert(modifier > 0, "Zoom cannot be set to 0");
+        if (currentZooming != null)
+            StopCoroutine(currentZooming);
+
+        currentZooming = ZoomCoroutine(modifier, durationZoom);
+        StartCoroutine(currentZooming);
+    }
+
+    IEnumerator ZoomCoroutine(float modifier , float duration)
+    {
+        float normalizedTime = 0;
+        if (modifierType == ZoomType.FOV)
+        {
+            float originValue = GetComponent<CinemachineVirtualCamera>().m_Lens.FieldOfView;
+            float targetValue = originValue * modifier;
+            while (normalizedTime < 1)
+            {
+                normalizedTime += Time.deltaTime / duration;
+                GetComponent<CinemachineVirtualCamera>().m_Lens.FieldOfView =
+                    Mathf.Lerp(originValue, targetValue, zoomOverTime.Evaluate(normalizedTime));
+                yield return null;
+            }
+
+            //Reset at end in editor mode
+            if (!Application.isPlaying)
+            {
+                GetComponent<CinemachineVirtualCamera>().m_Lens.FieldOfView = originValue;
+            }
+        }
+        else
+        {
+            float originValue = transposer.m_CameraDistance;
+            float targetValue = originValue * modifier;
+            while (normalizedTime < 1)
+            {
+                normalizedTime += Time.deltaTime / duration;
+                transposer.m_CameraDistance = Mathf.Lerp(originValue, targetValue, zoomOverTime.Evaluate(normalizedTime));
+                yield return null;
+            }
+
+            //Reset at end in editor mode
+            if (!Application.isPlaying)
+            {
+                transposer.m_CameraDistance = originValue;
+            }
+        }
+    }
+
+    #endregion
 }
