a08ceb6e3f509044eea68f9d0db5dfb7e773dbfe
Séverin Michaut <severinmichaut@gmail.com>
2019-11-18 19:58:56 +0100

    Health rework

--- TheLastBeatUnity/Assets/_Project/Scripts/Health.cs
+++ TheLastBeatUnity/Assets/_Project/Scripts/Health.cs
@@ -6,232 +6,76 @@ using Sirenix.OdinInspector;
 using DG.Tweening;
 using TMPro;
 
-public class Health : MonoBehaviour
+public class Health : Beatable
 {
-    #region properties
     [SerializeField]
     bool debugMode = false;
-    Rect debugWindowRect = new Rect(20, 20, 120, 50);
+    Rect debugWindowRect = new Rect(20, 20, 120, 80);
 
-    [TabGroup("Visual")] [SerializeField]
+    [SerializeField]
     Image healthBackground = null;
-    RectTransform healthBackgroundRect = null;
-    [TabGroup("Visual")] [SerializeField]
+
+    [SerializeField]
+    RectTransform healthBackgroundRect;
+
+    [SerializeField]
     TextMeshProUGUI healthText = null;
 
-    [TabGroup("Visual")]
     [SerializeField]
     [Range(1, 5)]
     int healthBackgroundNewScale = 0;
     float healthBackgroundCurrentScale = 0;
 
-    [TabGroup("Gameplay")] [SerializeField]
-    int startingFrequency = 0;
-    [TabGroup("Gameplay")] [SerializeField]
-    int minimalFrequency = 0;
-    [TabGroup("Gameplay")] [SerializeField]
-    int maximalFrequency = 0;
+    [SerializeField]
+    float numberBeat;
 
-    [TabGroup("Gameplay")] [SerializeField]
-    AnimationCurve hitCurve = null;
+    [SerializeField]
+    float losePerPulse;
 
-    [TabGroup("Gameplay")] [SerializeField]
-    MultiReference referenceMultiply = MultiReference.CurrentValue;
-
-    [TabGroup("Gameplay")] [SerializeField] [ValidateInput("Positive", "This value must be > 0")]
-    float freezeTime = 0;
-
-    [TabGroup("Gameplay")] [SerializeField] [ValidateInput("Positive", "This value must be > 0")]
-    float timeBeforeTachy = 0;
-    float currentTimeBeforeTachy = 0;
-    bool inTachycardie = false;
-
-    //Time stamp of the last action
-    float lastTimeAction = 0;
-    float beatsPerMinutes = 0;
-    float TimeBetweenBeats => (1 / beatsPerMinutes) * 60;
-    float DurationSequence => Mathf.Min(0.1f, TimeBetweenBeats / 2);
-    int numberBeat = 200;
-    float currentMultiplier = 1;
-    float accumulator = 0;
-    bool pause = false;
-
-    IEnumerator healthCoroutine = null;
-
-    public bool Positive(float value)
+    public override void Beat()
     {
-        return value > 0;
+        BeatSequence();
     }
 
-    public enum MultiReference
+    private void Start()
     {
-        OriginalValue,
-        CurrentValue
+        healthBackgroundCurrentScale = healthBackgroundRect.transform.localScale.x;
     }
-    #endregion
-
-    private void OnEnable()
-    {
-        EventManager.Instance.AddListener<PauseEvent>(OnPauseEvent);
-    }
-
-    private void OnDisable()
-    {
-        EventManager.Instance.RemoveListener<PauseEvent>(OnPauseEvent);
-    }
-
-    public void Start()
-    {
-        pause = false;
-        healthBackgroundRect = healthBackground.GetComponent<RectTransform>();
-        healthBackgroundCurrentScale = healthBackgroundRect.localScale.x;
-        beatsPerMinutes = startingFrequency;
-        Beat();
-    }
-
-    private void Update()
-    {
-        if (!pause)
-        {
-            accumulator += Time.deltaTime;
-            if (accumulator > TimeBetweenBeats)
-            {
-                accumulator = 0;
-                Beat();
-            }
-        }
-
-        //Heart Beat too high ! staying too long will trigger tachy mode
-        if (maximalFrequency < beatsPerMinutes)
-        {
-            currentTimeBeforeTachy -= Time.deltaTime;
-        }
-        else
-        {
-            currentTimeBeforeTachy = timeBeforeTachy;
-        }
-
-        //countdown just reached 0 , enter tachy mode
-        if (currentTimeBeforeTachy < 0 && ! inTachycardie)
-        {
-            SetTachycardie(true);
-        }
-    }
-
-    private void OnPauseEvent(PauseEvent e)
-    {
-        pause = e.pause;
-    }
-
-    public void Beat()
+    public void BeatSequence()
     {
         Sequence seqMin = DOTween.Sequence();
         Sequence seqMax = DOTween.Sequence();
 
-        seqMin.Append(healthBackgroundRect.DOScale(healthBackgroundNewScale, DurationSequence));
-        seqMin.Append(healthBackgroundRect.DOScale(healthBackgroundCurrentScale, DurationSequence));
+        seqMin.Append(healthBackgroundRect.DOScale(healthBackgroundNewScale, sequenceDuration));
+        seqMin.Append(healthBackgroundRect.DOScale(healthBackgroundCurrentScale, sequenceDuration));
         seqMin.Play();
 
-        seqMax.Append(healthBackgroundRect.DOScale(healthBackgroundNewScale, DurationSequence));
-        seqMax.Append(healthBackgroundRect.DOScale(healthBackgroundCurrentScale, DurationSequence));
+        seqMax.Append(healthBackgroundRect.DOScale(healthBackgroundNewScale, sequenceDuration));
+        seqMax.Append(healthBackgroundRect.DOScale(healthBackgroundCurrentScale, sequenceDuration));
         seqMax.Play();
 
-        numberBeat--;
+        numberBeat -= losePerPulse;
         healthText.text = numberBeat.ToString();
     }
 
-    public void Hit(float damage, float duration , bool multiply = true)
-    {
-        StartHealthTransition(beatsPerMinutes, CalculateNewValue(damage, false), duration);
-    }
-
-    public float CalculateNewValue(float value, bool multipy = true)
-    {
-        float output;
-        if (multipy)
-            output = (referenceMultiply == MultiReference.OriginalValue ? startingFrequency : beatsPerMinutes) * value;
-        else
-            output = beatsPerMinutes + value;
-
-        output = Mathf.Max(minimalFrequency, output);
-        return output;
-    }
-
-    //Add an action to the current combo
-    public void NewAction(float multiplier, float duration)
-    {
-        if (multiplier > currentMultiplier)
-        {
-            currentMultiplier = multiplier;
-            lastTimeAction = Time.timeSinceLevelLoad;
-            StartHealthTransition(beatsPerMinutes, CalculateNewValue(multiplier), 3);
-        }
-        else if ((int)multiplier == (int)currentMultiplier)
-        {
-            lastTimeAction = Time.timeSinceLevelLoad;
-        }
-        else
-        {
-            beatsPerMinutes = startingFrequency * multiplier;
-
-            //Freeze time over , heartBeat can down
-            if (Time.timeSinceLevelLoad - lastTimeAction > freezeTime)
-            {
-                lastTimeAction = Time.timeSinceLevelLoad;
-                currentMultiplier = multiplier;
-                StartHealthTransition(beatsPerMinutes, CalculateNewValue(multiplier), duration);
-            }
-        }
-    }
-
-    IEnumerator SetHealthCoroutine(float from, float to, float duration)
-    {
-        float normalizedTime = 0;
-        while (normalizedTime < 1)
-        {
-            normalizedTime += Time.deltaTime / duration;
-            beatsPerMinutes = Mathf.Lerp(from, to, hitCurve.Evaluate(normalizedTime));
-            yield return null;
-        }
-        healthCoroutine = null;
-    }
-
-    void StartHealthTransition(float from , float to, float duration)
-    {
-        Debug.Assert(duration > 0);
-        //Only one health animation at the same time
-        if (healthCoroutine != null)
-        {
-            StopCoroutine(healthCoroutine);
-        }
-        healthCoroutine = SetHealthCoroutine(from, to, duration);
-        StartCoroutine(healthCoroutine);
-    }
-
-    //What to do when you enter / leave tachycardie ?
-    public void SetTachycardie(bool value)
-    {
-        inTachycardie = value;
-        if (value)
-        {
-            healthBackground.color = Color.red;
-        }
-        else
-        {
-            healthBackground.color = Color.white;
-        }
-    }
-
-    public void OnGUI()
+    private void OnGUI()
     {
         if (debugMode)
         {
-            debugWindowRect = GUI.Window(0, debugWindowRect, DisplayWindow, "Debug");
+            debugWindowRect = GUI.Window(0, debugWindowRect, DebugWindow, "Debug");
         }
     }
 
-    void DisplayWindow(int id)
+    void DebugWindow(int windowID)
     {
-        GUI.Label(new Rect(70, 20, 50, 20),Mathf.Round(beatsPerMinutes).ToString());
+        if (GUI.Button(new Rect(30,25, 80, 25), "+"))
+        {
+            losePerPulse++;
+        }
+        
+        if (GUI.Button(new Rect(30, 50 , 80 , 25), "-"))
+        {
+            losePerPulse--;
+        }
     }
 }
