030ef434aa718d7492e9b5c5f49282444bac1a12
SeverinM <severinmichaut@gmail.com>
2019-11-21 20:54:30 +0100

    First implementation

---
+++ TheLastBeatUnity/Assets/_Project/Scripts/Camera/CameraArea.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraArea : MonoBehaviour
+{
+    [SerializeField]
+    string triggerEnter;
+
+    [SerializeField]
+    string triggerAtTag;
+
+    [SerializeField]
+    string triggerExit;
+
+    void OnTriggerEnter(Collider collid)
+    {
+        if (collid.CompareTag(triggerAtTag))
+        {
+            CameraManager.Instance.CameraStateChange(triggerEnter);
+        }
+    }
+
+    void OnTriggerExit(Collider collid)
+    {
+        if (collid.CompareTag(triggerAtTag))
+        {
+            CameraManager.Instance.CameraStateChange(triggerExit);
+        }
+    }
+}
---
+++ TheLastBeatUnity/Assets/_Project/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager Instance { get; private set; }
+
+    CinemachineStateDrivenCamera stateDrive;
+    Animator anim;
+
+    void Start()
+    {
+        stateDrive = GetComponent<CinemachineStateDrivenCamera>();
+        if (stateDrive)
+            anim = stateDrive.m_AnimatedTarget;
+
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    public void CameraStateChange(string triggerName)
+    {
+        Debug.Log(triggerName);
+        anim.SetTrigger(triggerName);
+    }
+
+}
