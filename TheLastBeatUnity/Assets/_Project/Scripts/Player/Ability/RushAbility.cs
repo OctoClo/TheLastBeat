b06709ee759cb0246fab7f4af8b85be1ea26e2db
Séverin <severinmichaut@gmail.com>
2019-12-13 16:41:47 +0100

    Turn integration

--- TheLastBeatUnity/Assets/_Project/Scripts/Player/Ability/RushAbility.cs
+++ TheLastBeatUnity/Assets/_Project/Scripts/Player/Ability/RushAbility.cs
@@ -18,6 +18,8 @@ public class RushParams : AbilityParams
     public Texture Texture = null;
     public float speedAnimMark = 0;
     public float markPersistDuration = 0;
+    public Texture turnVariante1 = null;
+    public Texture turnVariante2 = null;
 }
 
 public class RushAbility : Ability
@@ -57,7 +59,11 @@ public class RushAbility : Ability
         currentCooldown = cooldown;
         player.Status.StartDashing();
         player.Anim.LaunchAnim(EPlayerAnim.RUSHING);
-        CreateStartMark(player.transform.forward);
+
+        if (RewindRush.IsInCombo)
+            CreateTurnMark(player.transform.forward);
+        else
+            CreateStartMark(player.transform.forward);
 
         Sequence seq = DOTween.Sequence();
         Vector3 direction = new Vector3(player.CurrentTarget.transform.position.x, player.transform.position.y, player.CurrentTarget.transform.position.z) - player.transform.position;
@@ -97,6 +103,34 @@ public class RushAbility : Ability
             Vector3 finalPos = hit.point + (hit.normal * 0.001f);
 
             GameObject instanciatedTrail = GameObject.Instantiate(parameters.RushMark);
+            instanciatedTrail.transform.forward = player.transform.forward;
+            instanciatedTrail.transform.position = finalPos;
+            Material mat = instanciatedTrail.GetComponent<MeshRenderer>().material;
+            mat.SetFloat("_CoeffDissolve", 0);
+            mat.SetFloat("_ExtToInt", 0);
+            mat.SetTexture("_MainTex", Random.value < 0.5f ? parameters.turnVariante1 : parameters.turnVariante2);
+            mat.SetFloat("_BlurRatio", 0.25f);
+            mat.SetVector("_CenterUV", new Vector4(0.5f, 0, 0, 0));
+            mat.SetVector("_ToBorder", new Vector4(0.5f, 1, 0, 0));
+            Sequence seq = DOTween.Sequence();
+            seq.Append(DOTween.To(() => mat.GetFloat("_CoeffDissolve"), x => mat.SetFloat("_CoeffDissolve", x), 1, parameters.speedAnimMark));
+            seq.AppendInterval(parameters.markPersistDuration);
+            seq.Append(DOTween.To(() => mat.GetFloat("_CoeffDissolve"), x => mat.SetFloat("_CoeffDissolve", x), 0, parameters.speedAnimMark));
+            seq.AppendCallback(() => GameObject.Destroy(instanciatedTrail));
+            seq.Play();
+        }
+    }
+
+    void CreateTurnMark(Vector3 direction)
+    {
+        RaycastHit hit;
+        //Find nearest ground + can be created on steep
+        if (Physics.Raycast(player.CurrentFootOnGround.position, Vector3.down, out hit))
+        {
+            Vector3 finalPos = hit.point + (hit.normal * 0.001f);
+
+            GameObject instanciatedTrail = GameObject.Instantiate(parameters.RushMark);
+            instanciatedTrail.transform.localScale *= 0.7f;
             instanciatedTrail.transform.forward = player.transform.forward;
             instanciatedTrail.transform.position = finalPos;
             Material mat = instanciatedTrail.GetComponent<MeshRenderer>().material;
