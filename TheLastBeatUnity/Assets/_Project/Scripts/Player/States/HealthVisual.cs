60c699ec1350061ad0125fc1100eefb157676852
Séverin <severinmichaut@gmail.com>
2020-01-03 20:41:50 +0100

    No longer modify MainScene

--- TheLastBeatUnity/Assets/_Project/Scripts/Player/States/HealthVisual.cs
+++ TheLastBeatUnity/Assets/_Project/Scripts/Player/States/HealthVisual.cs
@@ -8,9 +8,19 @@ using DG.Tweening;
 public class VisualParams
 {
     public Image flameImage;
+    public Image barImage;
     public RectTransform flameTransform;
     public Animator riftAnimator;
     public float sequenceDuration;
+    public Color leftMostColor;
+    public Color rightMostColor;
+    public float ratioRiftStep1 = 0.7f;
+    public float ratioRiftStep2 = 0.5f;
+    public float ratioRiftStep3 = 0.2f;
+    public float pulseSize = 1.05f;
+    public float sizeCritic = 1.15f;
+    public Color hurtColor = Color.clear;
+    public AnimationCurve curveTransition;
 }
 
 public class HealthVisual
@@ -19,30 +29,45 @@ public class HealthVisual
     Sequence seq;
     Sequence criticSequence;
     Vector3 normalSize = Vector3.zero;
+    bool isChangingColor = false;
+    Color flameColor = Color.black;
 
     public HealthVisual(VisualParams vp)
     {
         visualParams = vp;
         normalSize = visualParams.flameTransform.localScale;
+        flameColor = visualParams.flameImage.color;
     }
 
-    public void RegularBeat(PulseZone zone)
+    public void HurtAnimationUI()
+    {
+        if (!isChangingColor)
+        {
+            isChangingColor = true;
+            Sequence seq = DOTween.Sequence();
+            seq.Append(visualParams.flameImage.DOColor(visualParams.hurtColor, 0.25f).SetEase(Ease.InOutSine));
+            seq.Append(visualParams.flameImage.DOColor(flameColor, 0.25f).SetEase(Ease.InOutSine));
+            seq.SetLoops(3);
+            seq.AppendCallback(() => isChangingColor = false);
+        }       
+    }
+
+    public void RegularBeat()
     {
         seq = DOTween.Sequence();
-        seq.Append(visualParams.flameTransform.DOScale(normalSize * zone.ScaleModifier, visualParams.sequenceDuration));
+        seq.Append(visualParams.flameTransform.DOScale(normalSize * visualParams.pulseSize, visualParams.sequenceDuration));
         seq.Append(visualParams.flameTransform.DOScale(normalSize,  visualParams.sequenceDuration));
         seq.Play();
     }
 
-    public void EnterCriticState(PulseZone zone)
+    public void EnterCriticState()
     {
-        visualParams.flameTransform.DOScale(normalSize * zone.ScaleModifier, 0.1f);
+        visualParams.flameTransform.DOScale(normalSize * visualParams.sizeCritic, 0.1f);
         criticSequence = DOTween.Sequence();
         criticSequence.Append(DOTween.To(() => visualParams.flameImage.color, x => visualParams.flameImage.color = x, Color.white, 0.1f));
-        criticSequence.Append(DOTween.To(() => visualParams.flameImage.color, x => visualParams.flameImage.color = x, zone.colorRepr, 0.1f));
+        criticSequence.Append(DOTween.To(() => visualParams.flameImage.color, x => visualParams.flameImage.color = x, Color.red, 0.1f));
         criticSequence.SetLoops(-1);
         criticSequence.Play();
-        visualParams.riftAnimator.SetInteger("indexState", 3);
     }
 
     public void ExitCriticState()
@@ -54,24 +79,24 @@ public class HealthVisual
         }
     }
 
-    public void SetRiftAnimation(int index, int listLength)
+    public void SetRiftAnimation(int index)
     {
-        if (index == listLength - 2)
-        {
-            visualParams.riftAnimator.SetInteger("indexState", 2);
-        }
-        else if (index == listLength - 3)
-        {
-            visualParams.riftAnimator.SetInteger("indexState", 1);
-        }
-        else
-        {
-            visualParams.riftAnimator.SetInteger("indexState", 0);
-        }
+        visualParams.riftAnimator.SetInteger("indexState", index);
     }
 
-    public void TransitionColor(Color newColor)
+    public void UpdateColor(float ratio)
     {
-        DOTween.To(() => visualParams.flameImage.color, x => visualParams.flameImage.color = x, newColor, 0.5f);
+        Color currentColor = Color.Lerp(visualParams.leftMostColor, visualParams.rightMostColor, ratio);
+        visualParams.barImage.DOColor(currentColor, 0.25f);
+        DOTween.To(() => visualParams.barImage.fillAmount, x => visualParams.barImage.fillAmount = x, ratio, 0.1f).SetEase(visualParams.curveTransition);
+
+        if (ratio < visualParams.ratioRiftStep1 && ratio > visualParams.ratioRiftStep2)
+            SetRiftAnimation(1);
+
+        if (ratio < visualParams.ratioRiftStep2 && ratio > visualParams.ratioRiftStep3)
+            SetRiftAnimation(2);
+
+        if (ratio < visualParams.ratioRiftStep3)
+            SetRiftAnimation(3);
     }
 }
