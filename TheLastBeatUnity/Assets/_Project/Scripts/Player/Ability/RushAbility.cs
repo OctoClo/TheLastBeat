34aed8b9ad9f1e2d207aec3cb82d0d88bb01dc0b
Claudia <octoclo@zoho.com>
2020-01-08 16:38:30 +0100

    Ennemies with shield

--- TheLastBeatUnity/Assets/_Project/Scripts/Player/Ability/RushAbility.cs
+++ TheLastBeatUnity/Assets/_Project/Scripts/Player/Ability/RushAbility.cs
@@ -132,6 +132,7 @@ public class RushAbility : Ability
         {
             direction *= -0.5f;
             goalPosition += direction;
+            seq.AppendCallback(() => player.Anim.SetRushing(false));
             seq.Append(player.transform.DOMove(goalPosition, parameters.RushDuration / 2.0f));
         }
 
