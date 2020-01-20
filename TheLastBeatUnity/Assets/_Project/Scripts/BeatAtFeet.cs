e9560706f5f6c869982f5000a719b0cdb482f637
Séverin <severinmichaut@gmail.com>
2020-01-20 18:06:56 +0100

    First integration

---
+++ TheLastBeatUnity/Assets/_Project/Scripts/BeatAtFeet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BeatAtFeet : Beatable
+{
+    [SerializeField]
+    Transform rootParent;
+
+    [SerializeField]
+    GameObject prefab;
+
+    [SerializeField]
+    Vector3 finalSize;
+
+    public override void Beat()
+    {
+        SoundManager sm = SoundManager.Instance;
+        float timeLeft = sm.LastBeat.beatInterval * 2.0f;
+        GameObject instantiated = Instantiate(prefab, rootParent);
+        instantiated.transform.localPosition = Vector3.zero;
+        Sequence seq = DOTween.Sequence()
+            .Append(instantiated.transform.DOScale(finalSize, timeLeft)).SetEase(Ease.Linear)
+            .Insert(0, DOTween.To(() => instantiated.GetComponent<MeshRenderer>().material.color, x => instantiated.GetComponent<MeshRenderer>().material.color = x, Color.white, timeLeft)).SetEase(Ease.Linear)
+            .AppendCallback(() => Destroy(instantiated));
+    }
+}
