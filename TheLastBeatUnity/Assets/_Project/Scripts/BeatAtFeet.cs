5d583308aa4ab3ef15aeb93ef030180d1ec1fe3d
Séverin <severinmichaut@gmail.com>
2020-01-20 18:33:22 +0100

    Doesnt work well

--- TheLastBeatUnity/Assets/_Project/Scripts/BeatAtFeet.cs
+++ TheLastBeatUnity/Assets/_Project/Scripts/BeatAtFeet.cs
@@ -17,12 +17,13 @@ public class BeatAtFeet : Beatable
     public override void Beat()
     {
         SoundManager sm = SoundManager.Instance;
-        float timeLeft = sm.LastBeat.beatInterval * 2.0f;
+        //float timeLeft = ((sm.LastBeat.lastTimeBeat + sm.LastBeat.beatInterval) - TimeManager.Instance.SampleCurrentTime()) + sm.LastBeat.beatInterval;
+        float timeLeft = (SoundManager.Instance.LastBeat.lastTimeBeat + SoundManager.Instance.LastBeat.beatInterval) - TimeManager.Instance.SampleCurrentTime();
+        Debug.Log(timeLeft);
         GameObject instantiated = Instantiate(prefab, rootParent);
         instantiated.transform.localPosition = Vector3.zero;
         Sequence seq = DOTween.Sequence()
             .Append(instantiated.transform.DOScale(finalSize, timeLeft)).SetEase(Ease.Linear)
-            .Append(DOTween.To(() => instantiated.GetComponent<MeshRenderer>().material.color, x => instantiated.GetComponent<MeshRenderer>().material.color = x, Color.clear, 0.2f).SetEase(Ease.Linear))
             .Insert(0, DOTween.To(() => instantiated.GetComponent<MeshRenderer>().material.color, x => instantiated.GetComponent<MeshRenderer>().material.color = x, Color.white, timeLeft).SetEase(Ease.Linear))
             .AppendCallback(() => Destroy(instantiated));
     }
