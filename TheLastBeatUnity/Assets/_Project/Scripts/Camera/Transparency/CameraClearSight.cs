09741b6f70daff1219b0db35f964483e4a55f069
Claudia <octoclo@zoho.com>
2020-01-28 14:22:02 +0100

    Simple transparency

--- TheLastBeatUnity/Assets/_Project/Scripts/Camera/Transparency/CameraClearSight.cs
+++ TheLastBeatUnity/Assets/_Project/Scripts/Camera/Transparency/CameraClearSight.cs
@@ -3,16 +3,23 @@ using Cinemachine;
 
 public class CameraClearSight : MonoBehaviour
 {
+    [Header("Transparency")][SerializeField]
+    float targetTransparency = 0.3f;
+    [SerializeField]
+    float fadeInTimeout = 0.2f;
+    [SerializeField]
+    float fadeOutTimeout = 0.2f;
+
+    [Header("General")][SerializeField]
+    bool debug = false;
     [SerializeField]
     Transform player = null;
     [SerializeField]
-    float fadeInTimeout = 0.6f;
-    [SerializeField]
-    float fadeOutTimeout = 0.2f;
-    [SerializeField]
-    float targetTransparency = 0.3f;
+    CapsuleCollider playerCapsule = null;
+
     CinemachineFramingTransposer transposer = null;
-    float distanceToPlayer = 5.0f; 
+    float distanceToPlayer = 5.0f;
+
     private void Start()
     {
         transposer = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineFramingTransposer>();
@@ -21,18 +28,36 @@ public class CameraClearSight : MonoBehaviour
 
     private void Update()
     {
-        Ray ray = new Ray(transform.position, player.transform.position - transform.position);
+        Vector3 capsuleTop = transform.position + playerCapsule.center + Vector3.up * playerCapsule.height * 0.5f;
+        Vector3 capsuleBottom = transform.position + playerCapsule.center - Vector3.up * playerCapsule.height * 0.5f;
+        Vector3 direction = player.position - transform.position;
 
-        RaycastHit[] hits = Physics.RaycastAll(ray, Vector3.Distance(player.position , transform.position));
+        if (debug)
+        {
+            Debug.DrawLine(capsuleTop, capsuleTop + direction, Color.cyan, 0.5f);
+            Debug.DrawLine(capsuleBottom, capsuleBottom + direction, Color.cyan, 0.5f);
+        }
+
+        RaycastHit[] hits = Physics.CapsuleCastAll(capsuleTop, capsuleBottom, playerCapsule.radius, direction, distanceToPlayer, ~(1 << LayerMask.NameToLayer("Enemies")));
         
         foreach (RaycastHit hit in hits)
         {
             Renderer renderer = hit.collider.GetComponent<Renderer>();
             
-            if (!renderer || hit.collider.isTrigger || hit.transform == player)
+            if (!renderer || hit.collider.isTrigger)
                 continue;
 
-            renderer.material.SetFloat("_Transparent", 0.2f);
+            // Add the script if not found
+            AutoTransparent autoTransparent = renderer.GetComponent<AutoTransparent>();
+            if (!autoTransparent)
+            {
+                autoTransparent = renderer.gameObject.AddComponent<AutoTransparent>();
+                autoTransparent.FadeInTimeout = fadeInTimeout;
+                autoTransparent.FadeOutTimeout = fadeOutTimeout;
+                autoTransparent.TargetTransparency = targetTransparency;
+            }
+
+            autoTransparent.BeTransparent();
         }
     }
 }
