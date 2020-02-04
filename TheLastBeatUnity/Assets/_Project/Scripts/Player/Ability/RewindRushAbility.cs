fb59f2f21b7e44336f2ee6a5753fa1edddc956c9
Séverin <severinmichaut@gmail.com>
2020-02-04 15:09:43 +0100

    Bug fix (at least i dont manage to reproduce it)

--- TheLastBeatUnity/Assets/_Project/Scripts/Player/Ability/RewindRushAbility.cs
+++ TheLastBeatUnity/Assets/_Project/Scripts/Player/Ability/RewindRushAbility.cs
@@ -33,6 +33,7 @@ public class RewindRushAbility : Ability
     RewindRushParameters parameters;
     float duration = 0;
     Sequence seq;
+    bool blockReset = false;
 
     public RewindRushAbility(RewindRushParameters rrp, float healCorrect) : base(rrp.AttachedPlayer, healCorrect)
     {
@@ -51,7 +52,7 @@ public class RewindRushAbility : Ability
         {
             rushChainTimer -= Time.deltaTime;
 
-            if (rushChainTimer < 0)
+            if (rushChainTimer < 0 && !blockReset)
                 ResetCombo();
         }
 
@@ -61,8 +62,8 @@ public class RewindRushAbility : Ability
 
     void RewindRush()
     {
+        blockReset = true;
         player.VisualPart.gameObject.SetActive(false);
-        SceneHelper.Instance.StartFade(() => { }, 0.2f, SceneHelper.Instance.ColorSlow);
         foreach(Enemy enn in GameObject.FindObjectsOfType<Enemy>())
         {
             enn.Timescale = 0;
@@ -149,7 +150,6 @@ public class RewindRushAbility : Ability
     public void AddChainEnemy(Enemy enn)
     {
         rushChainTimer = parameters.MaxTimeBeforeResetMarks;
-        enn.Informations.AddRewindMark();
 
         if (chainedEnemies.Count >= parameters.MaxChained)
         {
@@ -157,7 +157,10 @@ public class RewindRushAbility : Ability
         }
 
         if (chainedEnemies.Count(x => enn) < 3)
+        {
+            enn.Informations.AddRewindMark();
             chainedEnemies.Enqueue(enn);
+        }
     }
 
     public void MissInput()
@@ -177,10 +180,21 @@ public class RewindRushAbility : Ability
 
     public override void End()
     {
-        foreach(Enemy enn in chainedEnemies)
+        Dictionary<Enemy, int> allDamages = new Dictionary<Enemy, int>();
+        foreach (Enemy enn in chainedEnemies.ToList())
         {
-            if (enn)
-                enn.GetAttacked(attackOnRythm);
+            if (!enn)
+                continue;
+
+            if (allDamages.ContainsKey(enn))
+                allDamages[enn]++;
+            else
+                allDamages[enn] = 1;
+        }
+
+        foreach(Enemy enn in allDamages.Keys)
+        {
+            enn.GetAttacked(attackOnRythm, allDamages[enn]);
         }
 
         foreach (CameraEffect ce in CameraManager.Instance.AllCameras)
@@ -198,5 +212,6 @@ public class RewindRushAbility : Ability
         ResetCombo();
         parameters.NormalState.SetValue();
         player.VisualPart.gameObject.SetActive(true);
+        blockReset = false;
     }
 }
