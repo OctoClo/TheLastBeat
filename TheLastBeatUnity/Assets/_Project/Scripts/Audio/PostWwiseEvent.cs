166996d96407e1b4ea0577c2ab438d155e276b89
Jeremy Guarober <jeremyguarober@hotmail.fr>
2020-02-05 15:41:09 +0100

    NewEnemy SFX

---
+++ TheLastBeatUnity/Assets/_Project/Scripts/Audio/PostWwiseEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostWwiseEvent : MonoBehaviour
+{
+    [SerializeField]
+    AK.Wwise.Event attackEvent = null;
+    [SerializeField]
+    AK.Wwise.Event callAttackEvent = null;
+
+    public void PostAttack()
+    {
+        attackEvent.Post(gameObject);
+    }
+
+
+    public void PostCall()
+    {
+        callAttackEvent.Post(gameObject);
+    }
+}
