7539f4c56d95c07918a3b10a6ba1a5c806955c4b
Séverin <severinmichaut@gmail.com>
2020-02-06 14:01:38 +0100

    Health Cell integration

---
+++ TheLastBeatUnity/Assets/_Project/Scripts/Player/Health/LifeContainer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeContainer : MonoBehaviour
+{
+    [SerializeField]
+    GameObject slashAnimation;
+
+    [SerializeField]
+    Color hurtColor = Color.red;
+
+    [SerializeField]
+    Color healColor = Color.green;
+
+    public enum StateHealthCell
+    {
+        EMPTY,
+        HALF_EMPTY,
+        FULL
+    }
+
+    StateHealthCell currentState;
+    public StateHealthCell CurrentState
+    {
+        get
+        {
+            return currentState;
+        }
+        set
+        {
+            ComputeNewState(currentState, value);
+            currentState = value;
+        }
+    }
+
+    void ComputeNewState(StateHealthCell previous , StateHealthCell actual)
+    {
+
+    }
+}
