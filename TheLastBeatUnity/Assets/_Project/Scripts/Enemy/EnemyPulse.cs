07cc062e51d0ae726e15fb47e050a4d9c7dc2b8f
Claudia <octoclo@zoho.com>
2020-02-11 16:12:37 +0100

    New signs for enemy explosion

--- TheLastBeatUnity/Assets/_Project/Scripts/Enemy/EnemyPulse.cs
+++ TheLastBeatUnity/Assets/_Project/Scripts/Enemy/EnemyPulse.cs
@@ -5,6 +5,12 @@ using DG.Tweening;
 
 public class EnemyPulse : Beatable
 {
+    enum EPulseType
+    {
+        PULSE_ON_BEAT,
+        PULSE_FASTER_AND_FASTER
+    }
+
     [SerializeField]
     AnimationCurve curve = null;
     [SerializeField]
@@ -17,6 +23,8 @@ public class EnemyPulse : Beatable
     Color color = Color.white;
     List<Material> materials = new List<Material>();
     List<Sequence> sequences = new List<Sequence>();
+
+    EPulseType pulseType = EPulseType.PULSE_ON_BEAT;
 
     protected override void Start()
     {
@@ -32,18 +40,54 @@ public class EnemyPulse : Beatable
         countBeforeSetup--;
         if (countBeforeSetup <= 0)
         {
-            sequences.Clear();
-            foreach (Material mat in materials)
-            {
-                sequences.Add(DOTween.Sequence()
-                                .Append(DOTween.To(() => originValue, x => mat.SetVector("_EmissionColor", color * x), targetValue, sequenceDuration)
-                                    .SetEase(curve))
-                                .SetUpdate(true));
-            }
+            if (pulseType == EPulseType.PULSE_ON_BEAT)
+                LaunchEmission(sequenceDuration);
+        }
+    }
+
+    private void LaunchEmission(float duration)
+    {
+        sequences.Clear();
+        foreach (Material mat in materials)
+        {
+            sequences.Add(DOTween.Sequence()
+                            .Append(DOTween.To(() => originValue, x => mat.SetVector("_EmissionColor", color * x), targetValue, duration)
+                                .SetEase(curve))
+                            .SetUpdate(true));
+        }
+    }
+
+    public void PulseFasterAndFaster(float duration)
+    {
+        pulseType = EPulseType.PULSE_FASTER_AND_FASTER;
+        KillSequences();
+        StartCoroutine(PulseFasterAndFasterCoroutine());
+    }
+
+    private IEnumerator PulseFasterAndFasterCoroutine()
+    {
+        float intervalTime = 0.4f;
+        bool continueToPulse = true;
+        while (continueToPulse)
+        {
+            LaunchEmission(intervalTime);
+            
+            yield return new WaitForSeconds(intervalTime);
+            
+            intervalTime -= 0.04f;
+
+            if (intervalTime <= 0)
+                continueToPulse = false;
         }
     }
 
     private void OnDestroy()
+    {
+        StopAllCoroutines();
+        KillSequences();
+    }
+
+    private void KillSequences()
     {
         foreach (Sequence sequence in sequences)
         {
