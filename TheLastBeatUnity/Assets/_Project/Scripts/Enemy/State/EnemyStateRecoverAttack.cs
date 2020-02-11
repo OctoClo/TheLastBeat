0b4eb493c9d3e10d0345ce824a0ccd6c4b1d64cb
Claudia <octoclo@zoho.com>
2020-02-11 13:37:52 +0100

    Progressive agent radius on enemies after attack

--- TheLastBeatUnity/Assets/_Project/Scripts/Enemy/State/EnemyStateRecoverAttack.cs
+++ TheLastBeatUnity/Assets/_Project/Scripts/Enemy/State/EnemyStateRecoverAttack.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class EnemyStateRecoverAttack : EnemyState
 {
     float recoverDuration = 0;
     float recoverTimer = 0;
+    Sequence animation = null;
+    float defaultAgentRadius = 0;
 
     public EnemyStateRecoverAttack(Enemy newEnemy, float duration) : base(newEnemy)
     {
         stateEnum = EEnemyState.RECOVER_ATTACK;
         recoverDuration = duration;
+        defaultAgentRadius = enemy.Agent.radius;
+        enemy.EnemyKilled += () => { if (animation != null) animation.Kill(); };
     }
 
     public override void Enter()
@@ -30,6 +35,8 @@ public class EnemyStateRecoverAttack : EnemyState
 
     public override void Exit()
     {
+        enemy.Agent.radius = 0.1f;
         enemy.Agent.enabled = true;
+        DOTween.To(() => enemy.Agent.radius, x => enemy.Agent.radius = x, defaultAgentRadius, 0.8f);
     }
 }
