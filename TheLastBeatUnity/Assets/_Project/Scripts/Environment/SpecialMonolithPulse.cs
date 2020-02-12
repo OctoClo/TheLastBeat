bd00bf82c0813fc873fd8ce41f93dd2b9c445624
Claudia <octoclo@zoho.com>
2020-02-12 17:14:52 +0100

    Merge branch 'feature/End' into develop

---
+++ TheLastBeatUnity/Assets/_Project/Scripts/Environment/SpecialMonolithPulse.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SpecialMonolithPulse : Beatable
+{
+    [SerializeField]
+    AnimationCurve curve = null;
+
+    [SerializeField]
+    float targetValue = 5;
+    [SerializeField]
+    float newTargetValue = 10;
+    float originValue = 0;
+    
+    [SerializeField]
+    Color newEmissiveColor = Color.white;
+    Color emissiveColor = Color.white;
+    Sequence currentSeq = null;
+
+    int countBeforeSetup = 2;
+    Material mat = null;
+
+    protected override void Start()
+    {
+        base.Start();
+        mat = GetComponent<MeshRenderer>().material;
+        originValue = 0;
+        emissiveColor = mat.GetVector("_EmissionColor");
+    }
+
+    public override void Beat()
+    {
+        countBeforeSetup--;
+        if (countBeforeSetup <= 0)
+        {
+            currentSeq = DOTween.Sequence()
+                                    .Append(DOTween.To(() => originValue, x => mat.SetVector("_EmissionColor", emissiveColor * x), targetValue, sequenceDuration)
+                                    .SetEase(curve)).SetUpdate(true).Play();
+        }
+    }
+
+    public void ChangeColor()
+    {
+        currentSeq.Kill();
+        emissiveColor = newEmissiveColor;
+        targetValue = newTargetValue;
+        mat.SetVector("_EmissionColor", emissiveColor * newTargetValue);
+    }
+}
---
+++ TheLastBeatUnity/Assets/_Project/Scripts/HUD/ZoneNameAppear.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class ZoneNameAppear : MonoBehaviour
+{
+    [SerializeField]
+    GameObject zoneName = null;
+
+    [SerializeField]
+    float animDuration = 1;
+
+    public void Appear()
+    {
+        Image[] images = zoneName.GetComponentsInChildren<Image>();
+
+        Sequence seq = DOTween.Sequence().Pause();
+        foreach (Image image in images)
+            seq.Insert(0, image.DOFade(1, animDuration));
+        seq.Play();
+    }
+}
