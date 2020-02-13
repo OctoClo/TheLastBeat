9433d1212642b78909cd406bf09b8f70c980249e
Claudia <octoclo@zoho.com>
2020-02-13 13:29:22 +0100

    Main Menu done

---
+++ TheLastBeatUnity/Assets/_Project/Scripts/UI/ViewQuit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using Doozy.Engine.UI;
+
+public class ViewQuit : MonoBehaviour
+{
+    [SerializeField]
+    GameObject defaultButton = null;
+    UIButton defaultButtonUI = null;
+
+    [SerializeField]
+    GameObject returnButton = null;
+    UIButton returnButtonUI = null;
+
+    [SerializeField]
+    EventSystem eventSystem = null;
+
+    private void Awake()
+    {
+        defaultButtonUI = defaultButton.GetComponent<UIButton>();
+        returnButtonUI = returnButton.GetComponent<UIButton>();
+    }
+
+    public void OnShow()
+    {
+        eventSystem.SetSelectedGameObject(defaultButton);
+        defaultButtonUI.SelectButton();
+    }
+
+    public void OnHide()
+    {
+        eventSystem.SetSelectedGameObject(returnButton);
+        returnButtonUI.DeselectButton();
+        returnButtonUI.SelectButton();
+    }
+}
