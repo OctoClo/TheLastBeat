76e254cbb3c18a18bca00627dced16e846a20c13
Séverin <severinmichaut@gmail.com>
2020-02-13 10:53:04 +0100

    Integrated the whole base

--- TheLastBeatUnity/Assets/_Project/Scripts/Tuto/Tuto.cs
+++ TheLastBeatUnity/Assets/_Project/Scripts/Tuto/Tuto.cs
@@ -6,8 +6,28 @@ using UnityEngine.UI;
 
 public class Tuto : Inputable
 {
+    [Header("General")]
     [SerializeField]
-    Image firstImage = null;
+    Image backgroundImage = null;
+
+    [SerializeField]
+    Color backgroundColor = Color.black;
+
+    [Header("First step")]
+    [SerializeField]
+    Image textImage = null;
+
+    [Header("Second step")]
+    [SerializeField]
+    Image imageObtentionRewind = null;
+
+    [Header("Third step")]
+    [SerializeField]
+    Image videoFrame = null;
+
+    [Header("Fourth step")]
+    [SerializeField]
+    Sprite newSpriteVideo = null;
 
     Sequence blockSeq;
     int progression = 0;
@@ -24,6 +44,11 @@ public class Tuto : Inputable
         }
     }
 
+    private void Start()
+    {
+        textImage.rectTransform.localScale = Vector3.zero;
+    }
+
     public override void OnInputEnter()
     {
         Progression++;
@@ -31,18 +56,66 @@ public class Tuto : Inputable
 
     void InterpretNewProgression(int newValue)
     {
-        Debug.Log(newValue);
-        if (newValue == 2)
+        if (newValue == 1)
         {
-            DOTween.Sequence()
-                .AppendCallback(() =>
-                {
-                    firstImage.color = Color.clear;
-                    firstImage.rectTransform.localPosition = Vector3.down * 10.0f;
-                })
-                .Append(firstImage.rectTransform.DOMove(Vector3.up * 3, 1.0f).SetRelative(true))
-                .Insert(0 , firstImage.DOColor(Color.white, 1.0f));
+            TextAppear();
         }
+
+        switch (newValue)
+        {
+            case 1:
+                TextAppear();
+                break;
+
+            case 2:
+                ShowVideo();
+                break;
+
+            case 3:
+                videoFrame.sprite = newSpriteVideo;
+                break;
+
+            case 4:
+                End();
+                break;
+        }
+    }
+
+    void TextAppear()
+    {
+        Time.timeScale = 0;
+        IndependantSequence()
+            .AppendCallback(() => blockInput = true)
+            .Append(backgroundImage.DOColor(backgroundColor, 1))
+            .Append(textImage.rectTransform.DOScale(Vector3.one, 1))
+            .Insert(1, textImage.DOColor(Color.white, 1))
+            .AppendInterval(2.0f)
+            .Append(textImage.DOColor(new Color(1, 1, 1, 0), 1))
+            .Append(imageObtentionRewind.DOColor(Color.white, 1.0f))
+            .Insert(5, imageObtentionRewind.rectTransform.DOMove(Vector3.up * 10, 1).SetRelative(true))
+            .AppendCallback(() => blockInput = false);
+            
+    }
+
+    Sequence IndependantSequence()
+    {
+        return DOTween.Sequence().SetUpdate(true);
+    }
+
+    void ShowVideo()
+    {
+        IndependantSequence()
+            .Append(imageObtentionRewind.DOColor(new Color(1, 1, 1, 0), 1.0f))
+            .Append(videoFrame.DOColor(Color.white, 1.0f))
+            .SetUpdate(true);
+    }
+
+    void End()
+    {
+        IndependantSequence()
+            .Append(videoFrame.DOColor(new Color(1, 1, 1, 0), 1.0f))
+            .Insert(0 , backgroundImage.DOColor(Color.clear, 1.0f))
+            .AppendCallback(() => Time.timeScale = 1);
     }
 
     void TemporaryBlock(float time)
@@ -61,7 +134,6 @@ public class Tuto : Inputable
         if (player.GetButtonDown("Ok"))
         {
             Progression++;
-            TemporaryBlock(2.0f);
         }
     }
 }
